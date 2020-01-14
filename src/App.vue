07312d98c2f3998f53b7a55980dec80a1e33f4e2
Damilola Oluwafemi <damilolaoluwafemi@Damilolas-MacBook-Pro.local>
2020-01-14 09:37:22 +0100

    feat: homapage ui, and components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import { Bus } from './event bus/bus'
+
+export default {
+  data() {
+    return {
+    }
+  },
+
+  beforeDestroy() {
+    Bus.$off();
+  }
+}
+</script>
+
 
 <style lang="scss">
 #app {
@@ -15,18 +27,15 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
+  margin: 0;
 
-#nav {
-  padding: 30px;
+  // a {
+  //   font-weight: bold;
+  //   color: #2c3e50;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  //   &.router-link-exact-active {
+  //     color: #42b983;
+  //   }
+  // }
 }
 </style>
