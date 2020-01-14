654bd73a6196b114a63901e29643091477a8f765
Damilola Oluwafemi <damilolaoluwafemi@Damilolas-MacBook-Pro.local>
2020-01-14 09:39:02 +0100

    feat: states update, fetch weather from api

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,51 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    apiKey: process.env.VUE_APP_API_KEY,
+    apiUrl: process.env.VUE_APP_API_BASE,
+    weather: '',
+    temp: '',
+    location: '',
+    condition: '',
+    query: ''
   },
+  getters: {
+  },
+
   mutations: {
+    fetchWeather(state) {
+      axios.get(`${state.apiUrl}q=${state.query}&appid=${state.apiKey}`)
+      .then(res => {
+        state.weather = res.data;
+        state.condition = res.data['weather'][0]['main'];
+        state.temp = res.data['main']['temp'];
+        state.location = state.weather['name'] + ',' + state.weather['sys']['country'];
+      })
+      .catch(() => {
+        alert('Please check your input and try again')
+      });
+    },
+
+    updateQuery(state, payload) {
+      state.query = payload;
+    },
   },
+
   actions: {
+    requestWeather({ commit }) {
+      commit('fetchWeather');
+    },
+
+    patchQuery({ commit }, payload) {
+      commit('updateQuery', payload)
+    }
   },
+
   modules: {
   }
 })
